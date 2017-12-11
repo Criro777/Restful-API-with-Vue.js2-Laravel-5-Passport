578d5c67cf65d47b87531cb4e1954640675726df
Unknown <russiancardcollector@mail.ru>
2017-12-12 02:26:59 +0300

    Добавлена логика разграничения доступа в глобальном хуке Beforeeach

--- client_side/src/main.js
+++ client_side/src/main.js
@@ -5,15 +5,44 @@ import App from './App'
 import API from './API'
 import axios from 'axios'
 import router from './router'
+import AUTH from './components/auth/AUTH'
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000/';
-axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
+axios.defaults.headers.common['Authorization'] = 'Bearer ' + AUTH.getToken();
 
 window.axios = axios;
 window.API = API;
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+router.beforeEach(
+  (to, from, next) => {
+    if (to.matched.some(record => record.meta.forGuest)) {
+      if (AUTH.isAuthenticated()) {
+        next({
+          path: '/companies'
+        })
+      } else {
+        next()
+      }
+    }
+
+    else if (to.matched.some(record => record.meta.forAuth)) {
+      if (!AUTH.isAuthenticated()) {
+        next({
+          path: '/login'
+        })
+      } else {
+        next()
+      }
+    }
+
+    else {
+      next()
+    }
+  }
+)
 
 /* eslint-disable no-new */
 new Vue({
