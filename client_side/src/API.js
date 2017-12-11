3bca422935e030afe1e84661f289c2304d8ce793
Unknown <russiancardcollector@mail.ru>
2017-12-12 00:44:37 +0300

    добавлены параметры аутентификации

--- client_side/src/API.js
+++ client_side/src/API.js
@@ -1,6 +1,10 @@
 const API = {
   register: 'api/register',
   login: 'oauth/token',
+  companies: 'api/companies',
+  client_id: 2,
+  client_secret: 'sULlUJjYfidRm9lqJDPBp02VOFb69B2VkOEbQ6KE',
+  grant_type: 'password'
 
 };
 
