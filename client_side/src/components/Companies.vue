7f6f3e006d708bacc1f248ad1cf6d9a2eb44cf64
Unknown <russiancardcollector@mail.ru>
2017-12-12 12:58:01 +0300

    Добавлен компонент для отображения списка компаний

--- client_side/src/components/Companies.vue
+++ client_side/src/components/Companies.vue
@@ -4,7 +4,7 @@
       <div class="col-md-8 col-md-offset-2">
         <div class="panel panel-default">
           <div class="panel-heading text-left">
-            Companies v
+            The World’s Biggest Companies
           </div>
           <div class="panel-body table-responsive">
           </div>
@@ -16,35 +16,18 @@
           </div>
 
           <div class="panel panel-default">
-            <div class="panel-heading text-left">Companies List<a class="logout" @click ="logout()">Logout</a></div>
+            <div class="panel-heading text-left">Companies list<a class="logout" @click ="logout()">Logout</a></div>
             <div class="panel-body">
-              <button class="btn btn-info btn-sm" @click="showFilter = !showFilter">Filter by date</button>
-
-              <div class="filter text-center" v-if="showFilter">
-                <form @submit="OnFilter()">
-                  <div class="input-group date">
-                    <p>From</p>
-                  </div>
-
-                  <div class="input-group date">
-                    <p>To</p>
-
-                  </div>
-
-                  <div class="filter-btn">
-                    <button class="btn btn-primary btn-sm btn-block">Filter</button>
-                  </div>
-                </form>
-              </div>
+   
 
               <div v-if="!dataEmpty" class="data">
                 <table class="table table-bordered table-striped">
                   <thead>
                   <tr>
                     <th>Name</th>
-                    <th>Address</th>
+                    <th>Capital</th>
+                    <th>President</th>
                     <th>Website</th>
-                    <th>Email</th>
                     <th>Created</th>
                     <th width="100">&nbsp;</th>
                   </tr>
@@ -52,15 +35,25 @@
                   <tbody>
                   <tr v-for="company, index in companies">
                     <td>{{ company.name }}</td>
-                    <td>{{ company.address }}</td>
+                    <td>{{ company.capital }} $</td>
+                    <td>{{ company.president }}</td>
                     <td>{{ company.website }}</td>
-                    <td>{{ company.email }}</td>
                     <td>{{ company.created_at }}</td>
+                    <td><router-link :to="{name: 'CompanyEdit', params: {id: company.id}}" class="btn btn-xs btn-warning">
+                      Edit
+                    </router-link></td>
+
+                    <td><a href="#"
+                       class="btn btn-xs btn-danger"
+                       @click="deleteEntry(company.id, index)">
+                      Delete
+                    </a>
+                    </td>
                   </tr>
                   </tbody>
                 </table>
               </div>
-              <div v-if="dataEmpty" class="data-empty">
+              <div v-else="dataEmpty" class="data-empty">
                 <h2>Sorry, no matches were found for your query.</h2>
               </div>
             </div>
@@ -104,62 +97,35 @@
     },
 
     methods: {
-      OnFilter() {
-        let app = this;
-        let date_server = {
-          from: (app.date_from !== '') ? app.formatDate(app.date_from) : '',
-          to:   (app.date_to   !== '') ? app.formatDate(app.date_to)   : ''
-        };
-
-        axios.get(this.buildURL(date_server))
-          .then(function (resp) {
-            if (resp.data.length) {
-              app.companies = resp.data;
-            } else {
-              app.dataEmpty = true;
-            }
-            app.date_from = '';
-            app.date_to = '';
-
-          })
-          .catch(function (resp) {
-            alert("Could not load companies");
-          });
-      },
-
-      buildURL(date) {
-        return API.companies + "?from=" + date.from + "&to=" + date.to;
-      },
-
-      formatDate(date) {
-        return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
-      },
 
       logout() {
-
         AUTH.destroyToken();
         this.$router.replace('/');
+      },
 
+      deleteEntry(id, index) {
+        if (confirm("Do you really want to delete it?")) {
+          var app = this;
+          axios.delete(API.companies + id)
+            .then(function (resp) {
+              app.companies.splice(index, 1);
+            })
+            .catch(function (resp) {
+              alert("Could not delete company");
+            });
+        }
       }
     },
 
-    //   deleteEntry(id, index) {
-    //     if (confirm("Do you really want to delete it?")) {
-    //       var app = this;
-    //       axios.delete('/api/v1/companies/' + id)
-    //         .then(function (resp) {
-    //           app.companies.splice(index, 1);
-    //         })
-    //         .catch(function (resp) {
-    //           alert("Could not delete company");
-    //         });
-    //     }
-    //   }
   }
 </script>
 <style>
   #create {
     margin: 0 0 15px 5px;
+  }
+
+  .container{
+    width:100%;
   }
 
   .filter {
