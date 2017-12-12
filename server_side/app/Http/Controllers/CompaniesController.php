8280dae69b36230a547bd13d119f176de5131abc
Unknown <russiancardcollector@mail.ru>
2017-12-12 12:12:44 +0300

    Добавлен контроллер для CRUD-операций с моделью данных

---
+++ server_side/app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Company;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        if ($request->from && $request->to) {
+
+            return DB::table("companies")->select("companies.*")
+                ->where(DB::raw("Date(companies.created_at)"), '>=', $request->from)
+                ->where(DB::raw("Date(companies.created_at)"), '<=', $request->to)
+                ->get();
+
+        } else {
+
+            return Company::all();
+        }
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $company = Company::create($request->all());
+        return $company;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $company = Company::findOrFail($id);
+        $company->update($request->all());
+        return $company;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Company::findOrFail($id);
+        $company->delete();
+        return '';
+    }
+}
